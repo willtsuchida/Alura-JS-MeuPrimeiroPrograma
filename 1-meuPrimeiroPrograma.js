e19c913f5d92896ca8a406d03b164367d3822f19
willtsuchida <willian_hideki@hotmail.com>
2023-03-09 18:09:14 -0300

    Cap 1 finalizado. JavaScript: explorando a linguagem

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro programa: trabalhando com variaveis");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+
+const idadeSomada = idade + 2;
+
+console.log(idadeSomada)
