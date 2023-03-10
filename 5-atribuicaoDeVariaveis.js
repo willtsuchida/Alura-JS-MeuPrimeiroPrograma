eef1586ed66e7493437f35461414fbf8de8b1a6a
willtsuchida <willian_hideki@hotmail.com>
2023-03-10 11:15:33 -0300

    Cap 2 finalizado. JavaScript: explorando a linguagem

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 32;
+let nome = "Willian";
+const sobrenome = "Hideki";
+
+console.log(nome, sobrenome);
+//console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é: ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
